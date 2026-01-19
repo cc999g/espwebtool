a458f097b77f0247665828144ad29b7a0d2d795f
yc66 <57165327+cc999g@users.noreply.github.com>
2026-01-19 20:05:24 +0800

    更新 Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ import Button from '@mui/material/Button'
 import OpenInNewIcon from '@mui/icons-material/OpenInNew'
 import UploadIcon from '@mui/icons-material/Upload'
 import HelpIcon from '@mui/icons-material/Help'
+import StarIcon from '@mui/icons-material/Star' // 引入星星图标
 
 const Header = (props) => {
     return (
@@ -34,9 +35,7 @@ const Header = (props) => {
                 </Typography>
 
                 <Button
-                    //variant='contained'
-                    //color='success'
-                    sx={{ color: '#fff' }}
+                    sx={{ color: '#fff', mx: 0.5 }}
                     href='https://blog.spacehuhn.com/espcomm'
                     target='_blank'
                     endIcon={<HelpIcon />}>
@@ -44,13 +43,28 @@ const Header = (props) => {
                 </Button>
 
                 <Button
-                    //variant='contained'
-                    //color='success'
-                    sx={{ color: '#fff' }}
+                    sx={{ color: '#fff', mx: 0.5 }}
                     href='https://github.com/cc999g'
                     target='_blank'
                     endIcon={<OpenInNewIcon />}>
                     更多工具
+                </Button>
+
+                {/* 新增的 Star 按钮 - 调整了间距和样式 */}
+                <Button
+                    sx={{ 
+                        color: '#fff', 
+                        mx: 0.5,
+                        border: '1px solid #fff', // 加边框突出按钮
+                        borderRadius: 1,
+                        '&:hover': {
+                            backgroundColor: 'rgba(255,255,255,0.1)' // 鼠标悬浮浅背景
+                        }
+                    }}
+                    href='https://github.com/cc999g/espwebtool'
+                    target='_blank'
+                    endIcon={<StarIcon />}>
+                    给我点个star
                 </Button>
             </Toolbar>
         </AppBar>
